467dc627d3b0a6f2d9f7a6d89fc1608d14599283
GEORGIY ASLANYAN <13jeff07@gmail.com>
2024-05-08 21:47:05 +0500

    push changes

--- app/page.js
+++ app/page.js
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex w-[100vw] md:w-[70vw] text-xl xl:w-[54vw] min-h-screen flex-col items-center text-center justify-between gap-8">
-      <h1 className="  ">
-        Missed <span className="font-bold text-[#619e4e]">$FROGE?</span> <br/>Here is
-        your second chance.
-      </h1>
+    <main className="flex relative z-[1] w-[100vw] md:w-[70vw] text-xl xl:w-[54vw] min-h-svh flex-col items-center text-center justify-between gap-8">
+      <h1 className="jag text-7xl">Gud Meme</h1>
       <div className="w-full flex items-center justify-center relative overflow-hidden rounded-2xl ">
         <img
           src="/vingete.svg"
@@ -19,19 +16,23 @@ export default function Home() {
           playsInline
           muted
           loop
-          className="z-0 p-[2px] bg-[#141414] h-auto sm:h-[80vh] w-full sm:w-auto flex rounded-2xl">
+          className="z-0 p-[2px] bg-[#141414] h-auto sm:h-[80vh] w-full sm:w-auto flex rounded-2xl"
+        >
           <source src="/background.mp4" type="video/mp4" />
         </video>
       </div>
-      <p className="text-center px-2 sm:px-0">
-        When life gives you a second chance you <span className="font-bold text-[#619e4e]">take it!</span>
+      <p className="jag text-center text-4xl px-2 sm:px-0">
+      HI, Fren! I’m GM
       </p>
-      <p className="break-all w-[95%]">0xDA115F774b285FC345F18a5Ea451E560CB699E11</p>
+      <p className="break-all w-[95%]">
+        0xDA115F774b285FC345F18a5Ea451E560CB699E11
+      </p>
       <a
         className="bg-[#528D27] hover:bg-[#D9EA81] text-black px-8 ease-linear duration-100 transition-all  py-2 rounded-xl"
         href="https://app.uniswap.org/#/swap?outputCurrency=0xDA115F774b285FC345F18a5Ea451E560CB699E11"
         target="_blank"
-        rel="noopener noreferrer">
+        rel="noopener noreferrer"
+      >
         BUY NOW
       </a>
       <p>Tokenomics</p>
@@ -46,24 +47,32 @@ export default function Home() {
         className="bg-[#528D27] hover:bg-[#D9EA81] text-black ease-linear duration-100 transition-all px-4 py-2 rounded-xl"
         href="https://t.me/FROGE2VIP"
         target="_blank"
-        rel="noopener noreferrer">
-        JOIN COMMUNITY
+        rel="noopener noreferrer"
+      >
+       Join &quot;GM Squad&quot; and ape into $GM for a fun NOW FREN!
       </a>
       <div className="flex items-center sm:gap-10 gap-5">
-      <a
-            className="scale-100 hover:scale-110 ease-linear duration-150 transition-all"
-            href="https://www.dextools.io/app/en/ether/pair-explorer/0xDA115F774b285FC345F18a5Ea451E560CB699E11"
-            target="_blank"
-            rel="noopener noreferrer">
-            <Image alt="dextools" src={'/dextools.png'} width={40} height={40}/>
-          </a>
-          <a
-            className="scale-100 hover:scale-110  ease-linear duration-150 transition-all"
-            href="https://etherscan.io/token/0xDA115F774b285FC345F18a5Ea451E560CB699E11#code"
-            target="_blank"
-            rel="noopener noreferrer">
-             <Image alt="etherscan" src={'/etherscan.png'} width={40} height={40}/>
-          </a>
+        <a
+          className="scale-100 hover:scale-110 ease-linear duration-150 transition-all"
+          href="https://www.dextools.io/app/en/ether/pair-explorer/0xDA115F774b285FC345F18a5Ea451E560CB699E11"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image alt="dextools" src={"/dextools.png"} width={40} height={40} />
+        </a>
+        <a
+          className="scale-100 hover:scale-110  ease-linear duration-150 transition-all"
+          href="https://etherscan.io/token/0xDA115F774b285FC345F18a5Ea451E560CB699E11#code"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            alt="etherscan"
+            src={"/etherscan.png"}
+            width={40}
+            height={40}
+          />
+        </a>
       </div>
       <p className="text-xs text-gray-200">COPYRIGHT © 2023 FROGE 2.0</p>
     </main>
