df8e12c3077e3c99f33551cd59cb23adbff6d5cd
GEORGIY ASLANYAN <13jeff07@gmail.com>
2024-05-24 00:31:55 +0500

    hide adress

--- app/page.js
+++ app/page.js
@@ -31,11 +31,11 @@ export default function Home() {
         <span>Join</span> <span>&quot;GM&quot;</span> <span>and</span> <span>ape</span> <span>into</span> <span>$GM</span> <span>for</span> <span>a</span> <span> fun </span> <span>NOW </span> <span>FRENS!</span>       
       </p>
       <p className="break-all w-[95%] text-[#528D27]">
-      0x4206902Be961847906b610A90E57F049Ec3c3841
+      0x...
       </p>
       <a
         className="bg-[#528D27] text-[#141414] px-8 ease-linear duration-100 transition-all py-2 rounded-xl"
-        href="https://app.uniswap.org/swap?outputCurrency=0x4206902Be961847906b610A90E57F049Ec3c3841"
+        href="https://app.uniswap.org/swap?outputCurrency=0x..."
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -61,7 +61,7 @@ export default function Home() {
         
         <a
           className="scale-100 hover:scale-110  ease-linear duration-150 transition-all"
-          href="https://etherscan.io/token/0x4206902Be961847906b610A90E57F049Ec3c3841"
+          href="https://etherscan.io/token/0x..."
           target="_blank"
           rel="noopener noreferrer"
         >
